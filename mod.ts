91b44017b589a43c62accbe166dc838f8e7409dd
urielch <uriel.chemouni@gmail.com>
2023-06-05 08:59:35 +0300

    order icons by name

--- mod.ts
+++ mod.ts
@@ -17,8 +17,8 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/go":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-go@1.0.8/mod.ts",
- *     "react-icons/go/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-go@1.0.8/ico/",
+ *     "react-icons/go":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-go@1.0.9/mod.ts",
+ *     "react-icons/go/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-go@1.0.9/ico/",
  *   }
  * }
  * ```
@@ -26,7 +26,7 @@
  * ## Import an icon without import_map by and afer loading all icons from the lib go
  * 
  * ```ts
- * import { GoAlert } from "https://deno.land/x/react_icons_go@1.0.8/mod.ts"
+ * import { GoAlert } from "https://deno.land/x/react_icons_go@1.0.9/mod.ts"
  * ```
  * 
  * ## import_map import an icon from all icons
